4512b37e1ac8800955735fdc2916bdc66aca4c96
ptds106 <ptds106@gmail.com>
2020-02-06 17:03:54 -0800

    testing

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('redcups/<int:redcup_id>/', views.redcups_detail, name='detail'),
     path('redcups/create/', views.RedcupCreate.as_view(), name='redcups_create'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('redcups/<int:pk>/update/', views.RedcupUpdate.as_view(), name='redcup_update'),
+    path('redcups/<int:pk>/delete/', views.RedcupDelete.as_view(), name='redcup_delete'),
 ]
