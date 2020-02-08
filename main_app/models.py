20d9c101d81bc26688c82b88a37e1bfebd7a43b3
Bitajune <beheshtibita@gmail.com>
2020-02-07 18:30:05 -0800

    work on comment layout

--- main_app/models.py
+++ main_app/models.py
@@ -3,22 +3,26 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime
 
+
 class Redcup(models.Model):
     name = models.CharField(max_length=100)
-    rules = models.TextField(max_length=2000, default='Put your rules here!')
+    rules = models.TextField(max_length=2000, default="Put your rules here!")
     players = models.IntegerField(default=1)
-    link = models.URLField(max_length=200, default='https://www.youtube.com/watch?v=MpnTP5V5IfE')
+    link = models.URLField(
+        max_length=200, default="https://www.youtube.com/watch?v=MpnTP5V5IfE"
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'redcup_id': self.id})
+        return reverse("detail", kwargs={"redcup_id": self.id})
+
 
 class Comment(models.Model):
     created_date = models.DateTimeField(default=datetime.now, blank=True)
-    comment = models.CharField(max_length=1000, default="Enter comment here!")
+    comment = models.CharField(max_length=1000, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     redcup = models.ForeignKey(Redcup, on_delete=models.CASCADE)
 
@@ -26,4 +30,5 @@ class Comment(models.Model):
         return self.comment
 
     class Meta:
-        ordering = ['-created_date']
+        ordering = ["-created_date"]
+
