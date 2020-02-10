016b406a73d402f5d548fd1a61c227d6e2a1032c
Bitajune <beheshtibita@gmail.com>
2020-02-10 11:39:07 -0800

    21 authentication

--- redcup/urls.py
+++ redcup/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     # path('chat/', include('main_app.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("main_app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
