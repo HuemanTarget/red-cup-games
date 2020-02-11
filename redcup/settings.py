c9f8dadba48a8d72207d9731af442e1087cd21ac
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-02-11 15:24:27 -0800

    heroku redis

--- redcup/settings.py
+++ redcup/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 # import redis
 import django_heroku
 import os
-from secret import *
+# from secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
